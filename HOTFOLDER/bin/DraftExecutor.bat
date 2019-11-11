5262b9a70d2d034e0f22f6ccf68f0f93f9d58173
Nagarajaiah <196204@NTTDATA.COM>
2019-11-11 18:57:18 +0530

    Removed Hotfolder

--- HOTFOLDER/bin/DraftExecutor.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-D:\OpenText\OTMM\MediaManagement\bin\bulkimport.bat -user tsuper -password OT@dmin321 -mode xml -dir D:\eclipse-workspace\PriintPreProcessor\HOTFOLDER\DRAFT -folder 4f4a65e3174e9b1b17f12b089d811cf54db3ad93 -nodelete -count 25 >> D:\OpenText\OTMM\HotFolder\logs\ps-industrial-asset-import.log
-echo BulkImport for Draft files executed
